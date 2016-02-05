9e69886184aa910a31a436c09f44d7aea3a4aea6
kgkostadinov <kostadin.g.k@abv.bg>
2016-02-05 10:50:30 +0200

    Testing

---
+++ TDSTesting.Master/bin/Package_Debug/TDSTesting.Master/package/properties/addedfiles/scripts/ai.0.15.0-build58334.js
@@ -0,0 +1,2 @@
+﻿version=2.0
+xml=<?xml version="1.0" encoding="utf-8"?><addFile><collisionbehavior><overwriteExisting>true</overwriteExisting></collisionbehavior><path>scripts\ai.0.15.0-build58334.js</path><id>scripts\ai.0.15.0-build58334.js</id><parent></parent><file></file><hashCode>ZEsD1M+VVUsAccmukw8UHg==</hashCode></addFile>
